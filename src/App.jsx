9b9267883773fd49a2d795cc78e989da1f0ce0d4
joshua <josh@joshuadennis.ca>
2019-02-14 00:24:20 +0000

    socket server connect and displaying data in console

--- src/App.jsx
+++ src/App.jsx
@@ -24,16 +24,10 @@ class App extends Component {
   }
 
   componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+    this.chatSocket = new WebSocket("ws://localhost:3001");
+    this.chatSocket.onopen = (event) => {
+      this.chatSocket.send(JSON.stringify("Things are looking up!"));
+    };
   }
 
   addMessage(messageName, userName) {
@@ -44,12 +38,14 @@ class App extends Component {
     }
     // return newMessage;
     const messages = this.state.messages.concat(newMessage);
+    this.chatSocket.send(JSON.stringify(newMessage));
     this.setState({messages: messages});
   }
 
   handleKeyPress(event) {
     if (event.key === 'Enter') {
      this.addMessage(event.target.value, this.state.currentUser.name);
+     event.target.value = null;
     }
   }
 
@@ -60,7 +56,7 @@ class App extends Component {
           <a href="/" className="navbar-brand">Chatty McChatterton</a>
         </nav>
       <Message data={this.state.messages}/>
-      <ChatBar currentUser={this.state.currentUser.name} addMessage={this.handleSubmit} handleKeyPress={this.handleKeyPress} />
+      <ChatBar currentUser={this.state.currentUser.name} handleKeyPress={this.handleKeyPress} />
       </div>
     );
   }
